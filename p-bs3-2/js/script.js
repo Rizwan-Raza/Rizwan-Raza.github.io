411822139d7e39e88bbb8d2927d1c8955f98f9c9
Rizwan Raza <rizwan.raza987@gmail.com>
2018-07-21 12:17:41 +0530

    Added BS3

---
+++ p-bs3-2/js/script.js
@@ -0,0 +1,28 @@
+jQuery(function($) {
+
+	//#main-slider
+	$(function(){
+		$('#main-slider.carousel').carousel({
+			interval: 8000
+		});
+	});
+
+	$( '.centered' ).each(function( e ) {
+		$(this).css('margin-top',  ($('#main-slider').height() - $(this).height())/2);
+	});
+
+	$(window).resize(function(){
+		$( '.centered' ).each(function( e ) {
+			$(this).css('margin-top',  ($('#main-slider').height() - $(this).height())/2);
+		});
+	});
+
+	//goto top
+	$('.gototop').click(function(event) {
+		event.preventDefault();
+		$('html, body').animate({
+			scrollTop: $("body").offset().top
+		}, 500);
+	});	
+
+});
